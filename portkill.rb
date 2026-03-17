23cb17f3c6b3f1b5b709b3371e84c79d3e5c1d4c
Noman Ikram <nomanikram0@icloud.com>
2026-03-17 18:31:22 +0500

    Update sha256 checksum for portkill.rb

--- portkill.rb
+++ portkill.rb
@@ -2,7 +2,7 @@ class Portkill < Formula
   desc "Advanced port process manager"
   homepage "https://github.com/inomanikram/homebrew-portkill"
   url "https://github.com/iNomanIkram/homebrew-portkill/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "066a9ef8863660e4cbbb7783663a92d721c0cf7bfc81d9e4e591fb1b99cc0593"
+  sha256 "6e1c3a6a63190d6fe48e9182eb73b93bb42c26e389a5547ce2a2f71bf7b6a9c0"
   license "MIT"
 
   def install
