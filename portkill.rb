5cd8c8ed953805609b627319d1677f2415875744
Noman Ikram <nomanikram0@icloud.com>
2026-03-17 18:32:14 +0500

    Update URL to version 1.0.1 for Portkill

--- portkill.rb
+++ portkill.rb
@@ -1,7 +1,7 @@
 class Portkill < Formula
   desc "Advanced port process manager"
   homepage "https://github.com/inomanikram/homebrew-portkill"
-  url "https://github.com/iNomanIkram/homebrew-portkill/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/iNomanIkram/homebrew-portkill/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "6e1c3a6a63190d6fe48e9182eb73b93bb42c26e389a5547ce2a2f71bf7b6a9c0"
   license "MIT"
 
